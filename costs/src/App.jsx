c1f8c6cb7062e139e80c5a2521526eacff0f5d4e
Lucas P. Rodrigues <110637825+lucaspr159@users.noreply.github.com>
2023-01-12 09:05:46 -0300

    react

--- costs/src/App.jsx
+++ costs/src/App.jsx
@@ -4,25 +4,24 @@ import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
 import Container from './components/layout/Container'
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+import Projects from './components/pages/Projects'
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
+      <NavBar/>
       <Container customClass='minHeight'>
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/projects' element={<Projects />} />
           <Route exact path='/company' element={<Company />} />
           <Route exact path='/contact' element={<Contact />} />
           <Route exact path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/>
     </BrowserRouter>
   );
 }
